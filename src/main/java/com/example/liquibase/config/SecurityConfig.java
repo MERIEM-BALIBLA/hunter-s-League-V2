d42fae9c2e15c8d49d5cd2ace071b5720cee6beb
balibla meriem <balibla.meriem@gmail.com>
2024-12-26 11:45:17 +0100

    Configuration de Cors

--- src/main/java/com/example/liquibase/config/SecurityConfig.java
+++ src/main/java/com/example/liquibase/config/SecurityConfig.java
@@ -18,11 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -38,7 +34,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(customizer -> customizer.disable())
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(request -> request
 
                         .requestMatchers("api/users/**").hasRole("ADMIN")
@@ -57,19 +52,6 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.addAllowedOrigin("http://localhost:4200");  // Assurez-vous que l'URL de votre frontend est correcte
-//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // Méthodes autorisées
-//        config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));  // En-têtes autorisés
-//        config.setAllowCredentials(true);  // Si vous utilisez des cookies ou des tokens d'authentification
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", config);  // Appliquez la configuration à toutes les routes
-//        return source;
-//    }
 
 
     @Bean
--- src/main/java/com/example/liquibase/web/api/AuthenticationController.java
+++ src/main/java/com/example/liquibase/web/api/AuthenticationController.java
@@ -9,7 +9,6 @@ import com.example.liquibase.web.vm.mapper.RegisterVmMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 
